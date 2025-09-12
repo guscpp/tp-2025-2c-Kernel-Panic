e3523c005a1751b26587530cbac43e37c7e34cf1
utnso <pabloliu9@gmail.com>
2025-09-12 15:09:08 +0000

    agregue una funcion para que inicialice el config y el logger

--- storage/src/main.c
+++ storage/src/main.c
@@ -4,19 +4,60 @@ typedef struct {
     t_log* logger;
     t_config* config;
     int puerto;
-    bool fresh;
+    int fresh;
     char* punto_montaje;
     int retardo_operacion;
     int retardo_acceso_bloque;
     char* log_level;   
 }storage_t;
 
+storage_t* iniciar_storage(char* config_path){
+    storage_t* storage = malloc(sizeof(storage_t));
+
+    storage->logger = iniciar_logger("storage.log", "STORAGE", 1, LOG_LEVEL_INFO);
+
+    storage->config = iniciar_config(storage->logger, config_path);
+
+    storage->puerto = config_get_int_value(storage->config, "PUERTO_ESCUCHA");
+    storage->fresh = config_get_int_value(storage->config, "FRESH_START");
+    storage->punto_montaje = config_get_string_value(storage->config, "PUNTO_MONTAJE");
+    storage->retardo_operacion = config_get_int_value(storage->config, "RETARDO_ACCESO_OPERACION");
+    storage->retardo_acceso_bloque = config_get_int_value(storage->config, "RETARDO_ACCESO_BLOQUE");
+    storage->log_level = config_get_string_value(storage->config, "LOG_LEVEL");
+
+    log_info(storage->logger, "El storage se inicializo correctamente");
+
+    return storage;
+}
+
+void liberar_storage (storage_t* storage){
+    if(storage != NULL){
+        log_destroy(storage->logger);
+        config_destroy(storage->config);
+        free(storage->punto_montaje);
+        free(storage->log_level);
+        free(storage);
+    }
+
+}
 
 int main(int argc, char* argv[]) {
     
+    storage_t* storage = iniciar_storage(argv[1]);
+
+    log_info(storage->logger, "Puerto leido: %d", storage->puerto);
+    log_info(storage->logger, "Fresh leido: %d", storage->fresh);
+    log_info(storage->logger, "Punto de montaje leido: %s", storage->punto_montaje);
+    log_info(storage->logger, "Retardo operacion leido: %d", storage->retardo_operacion);
+    log_info(storage->logger, "Retardo acceso bloque leido: %d", storage->retardo_acceso_bloque);
+    log_info(storage->logger, "log level leido: %s", storage->log_level);
+
+    liberar_storage(storage);
+
+    return 0;
 }
 
-int inicializar_file_system(storage_t* storage){
+/*int inicializar_file_system(storage_t* storage){
     if(storage->fresh){
         formatear_fs(storage);
     }
@@ -29,5 +70,5 @@ int formatear_fs(storage_t* storage){
     return 0;
     
 }
-
+*/
 
--- storage/src/storage.h
+++ storage/src/storage.h
@@ -1,7 +1,10 @@
 #ifndef STORAGE_H
 #define STORAGE_H
 
-#include "../../utils/src/utils/inicializar.h"
-
+#include "../../utils/include/utils.h"
+#include "../../utils/include/sueltos.h"
+#include "../../utils/include/protocolo.h"
+#include "../../utils/include/paquete.h"
+#include "../../utils/include/conexion.h"
 
 #endif
